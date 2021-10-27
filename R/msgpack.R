615a17ee9247d21a48ec012e9d5574d4aeb23143
guigang xie <gg.xie@bionovogene.com>
2021-10-27 15:30:14 +0800

    add source projects

---
+++ R/msgpack.R
@@ -0,0 +1,22 @@
+
+#' Serialize any object into messagepack
+#' 
+#' @param x any ``R#`` object
+#' @param schema a schema for serialization object x to messagepack.
+#' 
+#' @return data stream object
+#' 
+const to_msgpack as function(x, schema = NULL) {
+
+}
+
+#' Deserialize object from messagepack data 
+#' 
+#' @param buffer the binary stream data
+#' 
+#' @return R# object that read from the binary stream 
+#'     data in messagepack data format.
+#' 
+const unpack as function(buffer) {
+
+}
